8686f9077756c9efb790e95908c7d3b5edf71e6d
MaksimPozdniakov <i@mpozdnjakov.ru>
2024-03-06 18:45:32 +0300

    11-th commit

--- app/src/main/java/project/gb/quizmaster/ui/main/WelcomeScreenFragment.kt
+++ app/src/main/java/project/gb/quizmaster/ui/main/WelcomeScreenFragment.kt
@@ -5,7 +5,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.activity.OnBackPressedCallback
 import androidx.navigation.fragment.findNavController
 import project.gb.quizmaster.R
 import project.gb.quizmaster.databinding.FragmentWelcomeScreenBinding
@@ -25,10 +24,6 @@ class WelcomeScreenFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        // Добавляем обратный вызов в обработчик нажатия кнопки "Back"
-        requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner, onBackPressedCallback)
-
-
         binding.startButton.setOnClickListener {
             findNavController().navigate(R.id.action_welcomeScreenFragment_to_surveyScreenFragment)
         }
@@ -37,18 +32,6 @@ class WelcomeScreenFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
-    }
-
-    private val onBackPressedCallback = object : OnBackPressedCallback(true) {
-        override fun handleOnBackPressed() {
-            // Перенаправляем на главный экран при нажатии кнопки "Back"
-            navigateToMainFragment()
-        }
-    }
-
-    private fun navigateToMainFragment() {
-        // Переходим на главный экран без добавления текущего фрагмента в стек навигации
-        findNavController().popBackStack(R.id.mainFragment, false)
     }
 
 }
